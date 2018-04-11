bef46b2260ed276f84d284ea9c68c4ee57c5b53a
Steve Sharman <steve@hackthorn.ai>
2018-04-11 22:45:48 +0100

    Add project files and update events spec

--- src/synergy_specs/events.clj
+++ src/synergy_specs/events.clj
@@ -3,15 +3,15 @@
             [clj-uuid :as uuid]))
 
 
-(s/def ::eventID uuid/uuid-string?)
+(s/def ::eventID string?)
 (s/def ::eventAction string?)
 (s/def ::eventData (s/map-of keyword? string?))
 (s/def ::eventVisible string?)
 (s/def ::eventStatus boolean?)
 
-(s/def ::userID uuid/uuid-string?)
-(s/def ::eventParent uuid/uuid-string?)
-(s/def ::originEvent uuid/uuid-string?)
+(s/def ::userID string?)
+(s/def ::eventParent string?)
+(s/def ::originEvent string?)
 (s/def ::eventTimestamp #(instance? Long %))
 
 (s/def ::inboundEvent (s/keys :req [
